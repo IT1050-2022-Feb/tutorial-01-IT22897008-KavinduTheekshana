a7904932c4e88885426fe8e14b24f9350e59c17d
IT22897008-KavinduTheekshana <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2023-04-01 13:06:35 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -1,6 +1,6 @@
 /*Exercise 3 - Repetition
 
-Write a C program to calculate the sum of the numbers from 1 to n.
+Write a C++ program to calculate the sum of the numbers from 1 to n.
 Where n is a keyboard input.
 
 e.g.
@@ -11,9 +11,18 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
-  
-  return 0;
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+
+	cout << "Enter Number :";
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		cout << " " << i + 1;
+	}
 }
 
