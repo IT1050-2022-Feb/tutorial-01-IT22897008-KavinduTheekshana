6557c56ada8ad3b397c3879d8596fdc7f2b16d29
IT22897008-KavinduTheekshana <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2023-04-01 12:47:47 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -1,6 +1,6 @@
 /*Exercise 2 - Selection
 
-Write a program to calculate the amount to be paid for a rented vehicle.
+Write a c++ program to calculate the amount to be paid for a rented vehicle.
 
 •	Input the distance the van has travelled
 •	The first 30 km is at a rate of 50/= per km.
@@ -15,8 +15,6 @@ Amount = 20 x 50 = 1000
 
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
-
-#include <stdio.h>
 
 #include<iostream>
 using namespace std;
@@ -37,4 +35,5 @@ int main()
 		amount = (distance - 30) * 40 + (30 * 50);
 	}
 	cout << "Full Payment is : " << amount;
+	return 0;
 }
