2305637dbe740f4a3947b4a2c37fcb5e80a2befe
IT22897008-KavinduTheekshana <127652289+IT22897008-KavinduTheekshana@users.noreply.github.com>
2023-04-01 12:41:04 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -2,10 +2,28 @@
 
    Write a C program to input marks of two subjects. Calculate and print the average of the two marks. */
 
-#include <stdio.h>
+#include<iostream>
+using namespace std;
 
-int main() {
-  
-  return 0;
+int main()
+{
+	int mark1, mark2,tot;
+	float avg;
+
+	cout << "Enter mark 1 : ";
+	cin >> mark1;
+	cout << "Enter mark 2 :";
+	cin >> mark2;
+
+	tot = mark1 + mark2;
+
+	cout << "Total mark is : " << tot<<endl;
+
+	avg = tot / 2;
+
+	cout << "Average is : " << avg<<endl;
+
+
+	return 0;
 }
 
